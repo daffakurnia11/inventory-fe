a524aa951c04ddeb1da2909fdf673dc68feab567
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-08-07 08:59:29 +0700

    feat: :sparkles: Add cookies expiration in env

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,32 +1,37 @@
-'use client';
+"use client";
 
-import { UserCookiesData } from '@/types/auth';
-import { setCookie, deleteCookie, getCookie } from 'cookies-next';
+import { UserCookiesData } from "@/types/auth";
+import { setCookie, deleteCookie, getCookie } from "cookies-next";
 
 export function getSession() {
-	const session = getCookie('session');
+  const session = getCookie("session");
 
-	const cookie = session ? JSON.parse(session) : '';
+  const cookie = session ? JSON.parse(session) : "";
 
-	return cookie;
+  return cookie;
 }
 
 export async function createSession(user: UserCookiesData) {
-	const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  // Expired in 1 day (Default)
+  const expiresAt = new Date(
+    Date.now() +
+      (Number(process.env.NEXT_PUBLIC_COOKIES_EXPIRATION) ||
+        24 * 60 * 60 * 1000)
+  );
 
-	setCookie('session', user, {
-		expires: expiresAt,
-	});
+  setCookie("session", user, {
+    expires: expiresAt,
+  });
 }
 
 export async function updateSession() {
-	const cookie = getSession();
+  const cookie = getSession();
 
-	if (!cookie) return null;
+  if (!cookie) return null;
 
-	createSession(cookie);
+  createSession(cookie);
 }
 
 export function deleteSession() {
-	deleteCookie('session');
+  deleteCookie("session");
 }
